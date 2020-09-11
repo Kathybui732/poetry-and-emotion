fe11c37672bcbf23371248bc013fbc27f06b9101
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-11 10:44:52 -0600

    Create ibm service spec and service all tests passing

--- spec/services/imb_service_spec.rb
+++ spec/services/imb_service_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Feature' do
   it "can get tone of poem" do
-    string = 'Not at Home to Callers'
-
-    tone_data = InternationalBusinessMachinesService.new.get_tone(string)
+    string = "Not at Home to Callers Says the Naked Tree -- Bonnet due in April -- Wishing you Good Day --"
+    results = InternationalBusinessMachinesService.new.get_tone(string)
+    expect(results).to have_key(:document_tone)
+    expect(results[:document_tone]).to have_key(:tones)
+    expect(results[:document_tone][:tones][0]).to have_key(:tone_name)
   end
 end
